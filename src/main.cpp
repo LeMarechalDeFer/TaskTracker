0aa12d979df386115261a482f179e0bbc14e3232
LeMarechalDeFer <blanchot@et.esiea.fr>
2025-04-02 03:32:57 +0700

    build: cmake & vpkg first intall

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// #include <nlohmann/json.hpp>
+#include <fmt/core.h>
+
+// using json = nlohmann::json;
+
+int main(){
+    // std::cout << "Hello world !" << std::endl;
+    fmt::print("Hello world\n");
+    return 0;
+}
