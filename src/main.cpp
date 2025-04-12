e28ce4524a5d59ca5b7c9d19189974498679322a
LeMarechalDeFer <blanchot@et.esiea.fr>
2025-04-12 16:59:45 +0700

    fix: doc & fix

--- src/main.cpp
+++ src/main.cpp
@@ -20,36 +20,64 @@
 
 int main(int argc, char** argv){
 
-    CLI::App app{"Hello world! Welcome to my TaskTrackerCLI, this project is for reviewing and practicing C++\n"};
-    
-    auto addTaskCmd = app.add_subcommand("add");
-    auto deleteTaskCmd = app.add_subcommand("delete");
-    auto editTaskCmd = app.add_subcommand("edit");
-    auto changeStatusCmd = app.add_subcommand("status");
-    auto printCmd = app.add_subcommand("print");
+    CLI::App app{
+        "📋 Hello world! Welcome to my TaskTrackerCLI, this project is for reviewing and practicing C++\n\n"
+        "Subcommands:\n"
+        "  ➤ add       Add a task (requires --description or -d)\n"
+        "  ➤ delete    Delete a task by ID (--id or -i)\n"
+        "  ➤ edit      Edit a task (requires --id and --description)\n"
+        "  ➤ status    Change status (requires --id and --status)\n"
+        "  ➤ print     Print tasks (optionally filtered by status)"
+    };
+    app.footer(R"(
+        Examples:
+        task add --description 'Buy milk'
+        task delete --id 1
+        task edit --id 1 --description 'Finish project'
+        task status --id 1 --status done
+        task print -s all
+        task print --status todo
+
+        Short options :
+        id: -i, status: -s, description: -d
+        Statuses allowed: all, todo, in-progress, done
+    )");
+
+    //need to add callbacks later for description printing
+    auto addTaskCmd = app.add_subcommand("add", "Add a new task to your list (format: add --description 'text' or -d 'text')");
+    auto deleteTaskCmd = app.add_subcommand("delete", "Delete an existing task (format: delete --id <num> or -i <num>)");
+    auto editTaskCmd = app.add_subcommand("edit", "Edit a task description (format: edit --id <num> --description 'text' or -i <num> -d 'text')");
+    auto changeStatusCmd = app.add_subcommand("status", "Change a task status (format: status --id <num> --status <status> or -i <num> -s <status>)");
+    auto printCmd = app.add_subcommand("print", "Display tasks (format: print --status <status> or -s <status>, options: all, todo, in-progress, done)");
 
     std::optional<std::string> description ;
-    addTaskCmd->add_option("--description", description, "Description of your task, don't forget ' ' ");
+    addTaskCmd->add_option("--description, -d", description, "Description of your task, don't forget ' ' ");
 
     std::optional<int> deleteID ; 
-    deleteTaskCmd->add_option("--id", deleteID, "Id to delete");
+    deleteTaskCmd->add_option("--id, -i", deleteID, "Id to delete");
 
     std::optional<int> updateID ;
     std::optional<std::string> updateDescription ;
-    editTaskCmd->add_option("--id", updateID, "Id to update");
-    editTaskCmd->add_option("--description", updateDescription, "Description to update");
+    editTaskCmd->add_option("--id, -i", updateID, "Id to update");
+    editTaskCmd->add_option("--description, -d", updateDescription, "Description to update");
     
     std::optional<int> statusID ;
     std::optional<std::string> newStatus ;
-    changeStatusCmd->add_option("--id", statusID, "Id to change status");
-    changeStatusCmd->add_option("--status", newStatus, "Status to update (todo, in-progress, done)")
+    changeStatusCmd->add_option("--id, -i", statusID, "Id to change status");
+    changeStatusCmd->add_option("--status, -s", newStatus, "Status to update (todo, in-progress, done)")
     ->check(CLI::IsMember({"all", "todo", "in-progress", "done"}));
 
     std::optional<std::string> printingByStatus ;
-    printCmd->add_option("--status", printingByStatus, "Printing by status (all, todo, in-progress, done)")
+    printCmd->add_option("--status, -s", printingByStatus, "Printing by status (all, todo, in-progress, done)")
     ->check(CLI::IsMember({"all", "todo", "in-progress", "done"}));
 
     CLI11_PARSE(app, argc, argv);
+
+    if (argc == 1) {
+    fmt::print("{}", app.help());
+    return 0;
+    }
+
 
     if(description.has_value()){
         TasksManager::addTask(description.value());
@@ -75,6 +103,8 @@ int main(int argc, char** argv){
         fmt::print("❌ Missing new status Id.\n💡 Format: status --id <yourID> --status 'your new status' (all, todo, in-progress, done)\n\n");
     }
 
+   
+
     if(printingByStatus.has_value()){
         if(printingByStatus.value() == "all"){
             TasksManager::printAllTasks();
@@ -83,7 +113,11 @@ int main(int argc, char** argv){
         }
     }
 
-}
+    
+};
+
+
+
 
 
 
